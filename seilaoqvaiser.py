0edddff0740c00e2e0a3ace517568798d19196cc
Luiz Carlos Silva de Araujo <lcsdafl.eng16@uea.edu.br>
2018-10-06 18:34:03 -0400

    update cbr

--- seilaoqvaiser.py
+++ seilaoqvaiser.py
@@ -4,6 +4,9 @@ Created on Sat Oct  6 16:11:56 2018
 
 @author: Luiz Carlos
 """
+
+import pandas as pd
+
 def verifyDirs(nextMove):
     if nextMove[0] < nextMove[3]: #ie "a1-b2" a<b
         hdir = 1
@@ -18,10 +21,32 @@ def verifyDirs(nextMove):
     return hdir, vdir
 
 class piece:
+    def __init__(self, kind):
+        self.dist = 1
+        self.dama = 0
+        self.kind = kind
+
+class board:
     def __init__(self):
-        dist = 1
-        dama = 0
+        checkers = [[piece('wp'), None,piece('wp'), None,piece('wp'), None,piece('wp'), None],
+                     [None, piece('wp'), None, piece('wp'), None, piece('wp'), None, piece('wp')],
+                     [piece('wp'), None,piece('wp'), None,piece('wp'), None,piece('wp'), None],
+                     [None, 'no', None, 'no', None, 'no', None, 'no'],
+                     ['no', None, 'no', None, 'no', None, 'no', None],
+                     [None, piece('bp'), None, piece('bp'), None, piece('bp'), None, piece('bp')],
+                     [piece('bp'), None,piece('bp'), None,piece('bp'), None,piece('bp'), None],
+                     [None, piece('bp'), None, piece('bp'), None, piece('bp'), None, piece('bp')]]
+    
+
+class CBR:
+    def __init__(self, caseBase):
+        self.caseBase = caseBase
+    
+    def similLocal:
+        
         
 
 if __name__ == "__main__":
-    pass
+    dataset = pd.read_csv(r"C:\Users\7\Desktop\smartchess\Case-Based-Reasoning-for-chess\checkersgame1.csv")
+    (h, v) = verifyDirs(dataset.loc[1,'next_move'])
+    
