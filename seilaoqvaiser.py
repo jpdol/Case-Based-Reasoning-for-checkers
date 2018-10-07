9416a46de2c4de08bee432bd61ab964c0d6d03c8
Luiz Carlos Silva de Araujo <lcsdafl.eng16@uea.edu.br>
2018-10-07 00:29:05 -0400

    Similaridade local, quissa global ta saindo do forno

--- seilaoqvaiser.py
+++ seilaoqvaiser.py
@@ -6,6 +6,7 @@ Created on Sat Oct  6 16:11:56 2018
 """
 
 import pandas as pd
+import numpy as np
 
 def verifyDirs(nextMove):
     if nextMove[0] < nextMove[3]: #ie "a1-b2" a<b
@@ -41,12 +42,77 @@ class board:
 class CBR:
     def __init__(self, caseBase):
         self.caseBase = caseBase
+        self.valueRange = [['no', 'wp', 'wd']]
+        self.globalSimList = []
     
-    def similLocal:
+    def simLocal(self, case, row, i):
+                
+        if row[i] == 'no':
+            ci = 0
+            weight = 1
+        elif row[i] == 'wp':
+            ci = 0.5
+            weight = 1
+        elif row[i] == 'wd':
+            ci = 1
+            weight = 1
+        elif row[i] == 'bp':
+            ci = -0.5
+            weight = 1
+        elif row[i] == 'bd':
+            ci = -1
+            weight = 1
+        elif i == len(row)-2:
+            ci = row[i]
+            weight = 7
+        elif i == len(row)-1:
+            ci = row[i]
+            weight = 5
+        else:
+            pass
         
+        if case[i] == 'no':
+            cf = 0
+            weight = 1
+        elif case[i] == 'wp':
+            cf = 0.5
+            weight = 1
+        elif case[i] == 'wd':
+            cf = 1
+            weight = 1
+        elif case[i] == 'bp':
+            cf = -0.5
+            weight = 1
+        elif case[i] == 'bd':
+            cf = -1
+            weight = 1
+        elif i == len(row)-2:
+            cf = row[i]
+            weight = 7
+        elif i == len(row)-1:
+            cf = row[i]
+            weight = 5
+        else:
+            pass
         
-
+        return np.max([0, 1 - np.abs(ci - cf)]), weight
+    
+    def simGlobal(self, case):
+        for index, row in self.caseBase.iterrows():
+            for i in range(len(row)):
+                (simLocal,weight) = self.simLocal(case, row, i)
+            
+        
+    
 if __name__ == "__main__":
     dataset = pd.read_csv(r"C:\Users\7\Desktop\smartchess\Case-Based-Reasoning-for-chess\checkersgame1.csv")
-    (h, v) = verifyDirs(dataset.loc[1,'next_move'])
+    #(h, v) = verifyDirs(dataset.loc[1,'next_move'])
+    out = pd.DataFrame(dataset.loc[:,'next_move'])
+    dataset = dataset.drop("next_move", axis = 1)
+    u = dataset.iterrows()
+    for index, row in dataset.iterrows():
+        for i in range(len(row)):
+            print(row[i])
+        print('----------')
+        
     
