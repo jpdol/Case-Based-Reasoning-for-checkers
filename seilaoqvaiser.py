4f0d69d91eb04a36bb21acc2b18b0a9af6fa9acf
Luiz Carlos Silva de Araujo <31112041+LuizCarlosS@users.noreply.github.com>
2018-10-08 23:33:49 -0400

    pau no meu cu

--- seilaoqvaiser.py
+++ seilaoqvaiser.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct  6 16:11:56 2018
-
 @author: Luiz Carlos
 """
 
 import pandas as pd
 import numpy as np
 import random as rd
-from winnerVerification import *
 
 def verifyDir(nextMove):
   if nextMove[0] < nextMove[3]: #ie "a1-b2" a<b
@@ -157,20 +155,16 @@ def verifyMovement(startPos, endPos, hDir, vDir, board, typeOfPiece):
   if board[startPos[0]][startPos[1]] == typeOfPiece and board[endPos[0]][endPos[1]] == 'no':
     return endPos
   elif board[startPos[0]][startPos[1]] == typeOfPiece and board[vDir*endPos[0]][hDir*endPos[1]] == 'no':
-    print([vDir*endPos[0], hDir*endPos[1]])
     return [vDir*endPos[0], hDir*endPos[1]]
   elif board[startPos[0]][startPos[1]] == typeOfPiece and board[vDir*endPos[0]][-hDir*endPos[1]] == 'no':
-    print([vDir*endPos[0], -hDir*endPos[1]])
     return [vDir*endPos[0], -hDir*endPos[1]]
   else:
-    print(3)
     return None
 
 def lookForPiece(board, startPos, hDir, vDir, typeOfPiece):
   i = 0
   j = 0
   board[startPos[0]][startPos[1]] = 'done'
-  print(board)
   while(i < 8):
     randDirline = rd.choice([-1,1])
     j = 0
@@ -307,11 +301,21 @@ class CBR:
     def avaliation(self,newCasesList, result):
       if result== 'win':
         att = 1.1
-        
+        for i in newCasesList:
+          i.append(0.75)
       elif result == 'lose':
         att = 0.9
+        for i in newCasesList:
+          i.append(0.25)
       else:
         att = 1
+        for i in newCasesList:
+          i.append(0.50)
+      for index in self.casesUsedList:
+          print(self.caseBase.loc[index]['jogada_otm'] * att)
+          #self.caseBase.loc[index]['jogada_otm'] = value
+      for case in newCasesList:
+        self.caseBase.loc[len(self.caseBase)] = case
       
     
     def receiveAndAdapt(self,case): #case eh uma lista
@@ -328,13 +332,12 @@ class CBR:
       index = 0
       if checkForCaptureP is not None:
         longestMove = checkForCaptureP[0]
-        # print(checkForCaptureP)
         for i, out in enumerate(checkForCaptureP):
           if len(out) > len(longestMove):
             longestMove = out
             index = i
       if checkForCaptureD is not None:
-        longestMove = checkForCaptureD[0]
+        longestMove = checkForCaptureP[0]
         for i, out in enumerate(checkForCaptureD):
           if len(out) > len(longestMove):
             longestMove = out
@@ -354,8 +357,6 @@ class CBR:
       self.casesUsedList.append(index)
       outB = self.out.loc[index][0]
       startPos = [ord(outB[0]) - 97, int(outB[1]) - 1]
-      outB = self.out.loc[index][0]
-      startPos = [ord(outB[0]) - 97, int(outB[1]) - 1]
       endPos = [ord(outB[3]) - 97, int(outB[4]) - 1]
       typeOfPiece = self.caseBase.loc[index][outB[0]+outB[1]]
       hDir, vDir = verifyDir(outB)
@@ -368,64 +369,17 @@ class CBR:
         
         
 if __name__ == "__main__":
-    dataset = pd.read_csv(r"CaseBase/caseBase.csv")
+    dataset = pd.read_csv(r"C:\Users\luido\OneDrive\Documentos\GitHub\Case-Based-Reasoning-for-checkers\CaseBase\caseBase.csv")
     #(h, v) = verifyDirs(dataset.loc[1,'next_move'])
     out = pd.DataFrame(dataset.loc[:,'next_move'])
     dataset = dataset.drop("next_move", axis = 1)
+    dataset = dataset.drop("jogada_otm", axis = 1)
     cbr = CBR(dataset, out)
-
-    flag = True
-    #m = cbr.adapt(['wp',None,'wp',None,'wp',None,'wp',None,None,'wp',None,'wp',None,'wp',None,'wp','wp',None,'no',None,'wp',None,'wp',None,None,'wp',None,'no',None,'no',None,'no','no',None,'no',None,'bp',None,'no',None,None,'bp',None,'no',None,'bp',None,'bp','bp',None,'bp',None,'bp',None,'bp',None,None,'bp',None,'bp',None,'bp',None,'bp',0,0.75])
+    #m = cbr.receiveAndAdapt(['wp',None,'wp',None,'wp',None,'wp',None,None,'wp',None,'wp',None,'wp',None,'wp','wp',None,'no',None,'wp',None,'wp',None,None,'wp',None,'no',None,'no',None,'no','no',None,'no',None,'bp',None,'no',None,None,'bp',None,'no',None,'bp',None,'bp','bp',None,'bp',None,'bp',None,'bp',None,None,'bp',None,'bp',None,'bp',None,'bp',0,0.75])
     #cbr.adapt(['wp',None,'wp',None,'wp',None,'wp',None,None,'wp',None,'wp',None,'wp',None,'wp','wp',None,'no',None,'wp',None,'wp',None,None,'wp',None,'no',None,'no',None,'no','no',None,'no',None,'bp',None,'no',None,None,'bp',None,'no',None,'bp',None,'bp','bp',None,'bp',None,'bp',None,'bp',None,None,'bp',None,'bp',None,'bp',None,'bp',0,0.75])
     #k = cbr.adapt(['wp',None,'wp',None,'wp',None,'wp',None,None,'wp',None,'wp',None,'wp',None,'wp','wp',None,'no',None,'wp',None,'wp',None,None,'bp',None,'no',None,'no',None,'no','bp',None,'no',None,'no',None,'no',None,None,'no',None,'no',None,'bp',None,'bp','bp',None,'bp',None,'bp',None,'bp',None,None,'bp',None,'bp',None,'bp',None,'bp',1,0.75])
     #l = cbr.adapt(['no',None,'wp',None,'wp',None,'wp',None,None,'no',None,'no',None,'no',None,'no','wp',None,'bp',None,'no',None,'no',None,None,'no',None,'no',None,'bp',None,'no','no',None,'no',None,'no',None,'no',None,None,'no',None,'wd',None,'bp',None,'bp','no',None,'no',None,'no',None,'bp',None,None,'no',None,'no',None,'no',None,'bp',1,0.75])
-
     n = cbr.receiveAndAdapt(['no',None,'wp',None,'wp',None,'no',None,None,'wp',None,'wp',None,'no',None,'wp','no',None,'wp',None,'no',None,'wp',None,None,'bp',None,'no',None,'wp',None,'wp','bp',None,'no',None,'bp',None,'no',None,None,'no',None,'bp',None,'bp',None,'bp','bp',None,'no',None,'no',None,'no',None,None,'no',None,'bp',None,'no',None,'bp',0,0.75])
-
-    boards_list = []
-    while(flag):
-      print("Por favor entre com o estado atual do tabuleiro: ")
-      current_board = str(input())
-      current_board = current_board.replace("'",'')
-      current_board = current_board.split(",")
-      i = 0
-      while(i<len(current_board)):
-        if current_board[i] == "None":
-          current_board[i] = None
-        i+=1
-      next_move = cbr.adapt(current_board)
-
-      print("Movimento recomendado: "+ next_move)
-
-      find_x = next_move.find("x")
-      temp = current_board
-      if find_x != -1:
-        temp.append(1)
-      else:
-        temp.append(0)
-
-      temp.append(next_move)
-      temp.append(0.75)
-      boards_list.append(temp)
-
-      result = winnerVerification(current_board, next_move)
-
-      if result == 'white':
-        print("Você venceu")
-        # Fazer o lance do avaliation aqui
-        flag = False
-      elif result == 'black':
-        print("Você perdeu")
-        flag = False
-      elif result == "match tied":
-        print("Empate")
-        flag = False
-      else:
-        pass
-
-    # n = cbr.adapt(current_board)
-    # n = cbr.adapt(['no',None,'wp',None,'wp',None,'no',None,None,'wp',None,'wp',None,'no',None,'wp','no',None,'wp',None,'no',None,'wp',None,None,'bp',None,'no',None,'wp',None,'wp','bp',None,'no',None,'bp',None,'no',None,None,'no',None,'bp',None,'bp',None,'bp','bp',None,'no',None,'no',None,'no',None,None,'no',None,'bp',None,'no',None,'bp',0,0.75])
-    # print(n)
-
+    cbr.avaliation([['no',None,'wp',None,'wp',None,'no',None,None,'wp',None,'wp',None,'no',None,'wp','no',None,'wp',None,'no',None,'wp',None,None,'bp',None,'no',None,'wp',None,'wp','bp',None,'no',None,'bp',None,'no',None,None,'no',None,'bp',None,'bp',None,'bp','bp',None,'no',None,'no',None,'no',None,None,'no',None,'bp',None,'no',None,'bp',0,0.75]], 'win')
     
     
